0751da9b780a61ee41540d839ce6dec86dd4678a
caio-caldas <caio.caldas@agenciaeconverse.com.br>
2020-04-19 16:39:45 -0300

    feat: react challenge done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState( [] );
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      console.log(response.data);
+      setRepositories(response.data);
+    });
+  }, []);
+
+  // add a new repository
   async function handleAddRepository() {
-    // TODO
+    const searchbar = document.getElementById('addInput');
+    
+    const response = await api.post('repositories', {
+      title: `${searchbar.value}`,
+      url: "https://github.com/cdvlopr",
+      techs: ["Sass", "Node.js", "React"],
+    });
+  
+    searchbar.value = '';
+
+    const newrepository = response.data;
+
+    setRepositories( [...repositories, newrepository] );
   }
 
+  // remove repository
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    const response = repositories.filter(repository => {
+      return repository.id !== id;
+    });
+
+    setRepositories(response);
   }
 
   return (
-    <div>
+    <>
+      <label>Cadastrar Repositório:</label>
+      <div className="input-center">
+        <input
+          type="text"
+          required
+          id="addInput"
+          placeholder="Please, type something!"
+          />
+
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
+
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {
+          repositories.map(repository => (
+              <li key={repository.id}>
+                {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+                <button onClick={() => handleRemoveRepository(repository.id)}>
+                  Remover
+                </button>
+              </li>
+            )
+          )
+        }
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+    </>
   );
 }
 
